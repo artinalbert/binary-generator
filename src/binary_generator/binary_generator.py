370fe8ea1866b8e47d66c3ada2bdcb5b93788987
Amirhossein Dabaghmanesh <58168246+artinalbert@users.noreply.github.com>
2022-04-14 14:55:34 +0200

    binary generator...

--- src/binary_generator/binary_generator.py
+++ src/binary_generator/binary_generator.py
@@ -1,2 +1,22 @@
 from number_lib.conversions import binary_to_octal
-def
+import random
+
+
+def binary_generator(n):
+    p = ''
+    for i in range(p):
+        temp = str(random.randint(0, 1))
+
+        # Concatenation the random 0, 1
+        # to the final result
+        p += temp
+
+    binary_to_octal(p)
+
+    return p
+
+
+ex = 7
+example = binary_generator(ex)
+
+print(f'Here is the random number: {example}')
